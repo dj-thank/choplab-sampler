de00c2b3b6c51c81a39099be0b03377daa34fa8c
ChopLab Scaffold <scaffold@local.invalid>
2026-08-24 02:13:20 +0900

    fix(supply-chain): bind SBOM to release identity

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.cyclonedx.model.Component
+
 plugins {
     id("org.cyclonedx.bom") version "3.4.1"
     id("com.android.application") version "9.3.1" apply false
@@ -6,4 +8,25 @@ plugins {
     id("org.jetbrains.kotlin.multiplatform") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("org.jetbrains.compose") version "1.11.1" apply false
+}
+
+val choplabVersion = providers.gradleProperty("choplabVersion").orElse("0.0.0-dev")
+
+allprojects {
+    group = "com.choplab"
+    version = rootProject.providers.gradleProperty("choplabVersion").orElse("0.0.0-dev").get()
+}
+
+tasks.cyclonedxDirectBom {
+    projectType = Component.Type.APPLICATION
+    componentGroup = "com.choplab"
+    componentName = "ChopLab"
+    componentVersion = choplabVersion.get()
+}
+
+tasks.cyclonedxBom {
+    projectType = Component.Type.APPLICATION
+    componentGroup = "com.choplab"
+    componentName = "ChopLab"
+    componentVersion = choplabVersion.get()
 }
